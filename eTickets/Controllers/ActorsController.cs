b05cb1482837a41a9df9d80069f2122d3cba01f2
vasiliv <32122468+vasiliv@users.noreply.github.com>
2022-12-27 10:04:40 +0400

    4-2 finished

---
+++ eTickets/Controllers/ActorsController.cs
@@ -0,0 +1,19 @@
+﻿using eTickets.Data;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace eTickets.Controllers
+{
+    public class ActorsController : Controller
+    {
+        private readonly AppDbContext _context;
+        public ActorsController(AppDbContext context)
+        {
+            _context= context;
+        }
+        public IActionResult Index()
+        {
+            return View(_context.Actors.ToList());
+        }
+    }
+}
