53c8478931d72356b900747f9d7fbe3ea135e9bb
vasiliv <32122468+vasiliv@users.noreply.github.com>
2022-12-27 11:19:15 +0400

    4-5 finished

--- eTickets/Controllers/MoviesController.cs
+++ eTickets/Controllers/MoviesController.cs
@@ -5,16 +5,16 @@ using System.Threading.Tasks;
 
 namespace eTickets.Controllers
 {
-    public class CinemasController : Controller
+    public class MoviesController : Controller
     {
         private readonly AppDbContext _context;
-        public CinemasController(AppDbContext context)
+        public MoviesController(AppDbContext context)
         {
             _context = context;
         }
         public async Task<IActionResult> Index()
         {
-            return View(await _context.Cinemas.ToListAsync());
+            return View(await _context.Movies.ToListAsync());
         }
     }
 }
