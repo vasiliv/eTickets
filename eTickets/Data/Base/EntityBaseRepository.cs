b26fc0cfe25e9d9c9bd66c14723f281ac3ead9c3
vasiliv <32122468+vasiliv@users.noreply.github.com>
2023-01-03 19:54:09 +0400

    7-1 finished

---
+++ eTickets/Data/Base/EntityBaseRepository.cs
@@ -0,0 +1,34 @@
+﻿using eTickets.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace eTickets.Data.Base
+{
+    public class EntityBaseRepository<T> : IEntityBaseRepository<T> where T : class, IEntityBase, new()
+    {
+        public async Task AddAsync(T entity)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public async Task<Actor> GetByIdAsync(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public async Task<T> UpdateAsync(int id, T entity)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ eTickets/Data/Base/IEntityBaseRepository.cs
@@ -0,0 +1,18 @@
+﻿using eTickets.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace eTickets.Data.Base
+{
+    //the "new()" constraint specifies that the type argument must have a public parameterless
+    //constructor. This means that when you use this interface, the type argument must be a class that
+    //implements the "IEntityBase" interface and also has a public parameterless constructor.
+    public interface IEntityBaseRepository<T> where T : class, IEntityBase, new()
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<Actor> GetByIdAsync(int id);
+        Task AddAsync(T entity);
+        Task<T> UpdateAsync(int id, T entity);
+        Task DeleteAsync(int id);
+    }
+}
