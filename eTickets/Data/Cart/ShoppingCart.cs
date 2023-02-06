290bfb4bf351229cd134f9ff6c24d3cb0e19803b
vasiliv <32122468+vasiliv@users.noreply.github.com>
2023-02-06 15:43:34 +0400

    11-3 finished

---
+++ eTickets/Data/Cart/ShoppingCart.cs
@@ -0,0 +1,29 @@
+﻿using eTickets.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace eTickets.Data.Cart
+{
+    //add or remove items to shopping cart
+    public class ShoppingCart
+    {
+        private readonly AppDbContext _context;
+        public string ShoppingCartId { get; set; }
+        public List<ShoppingCartItem> ShoppingCartItems { get; set; }
+        public ShoppingCart(AppDbContext context)
+        {
+            _context= context;
+        }
+        public List<ShoppingCartItem> GetShoppingCartItems()
+        {
+            // ?? - if ShoppingCartItems == null
+            return ShoppingCartItems ?? (ShoppingCartItems = _context.ShoppingCartItems
+                                        .Where(n => n.ShoppingCartId == ShoppingCartId)
+                                        .Include(n => n.Movie).ToList());
+        }
+        public double GetShoppingCartTotal() => _context.ShoppingCartItems
+                                                .Where(n => n.ShoppingCartId == ShoppingCartId)
+                                                .Select(n => n.Movie.Price * n.Amount).Sum();        
+    }
+}
